69960e498a28e3849392974243cf36e56721be4d
Abhishek Kumar <ahsekkumar@gmail.com>
2025-11-21 21:28:48 +0530

    fetched api from omdb

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 import './App.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { useState } from 'react';
+
+const [getMovies,setGetMovies]= useState("");
+
+const apiKey= '875daaf7';
+async function getMovie(name) {
+    try {
+      let url = `https://www.omdbapi.com/?apikey=${apiKey}&s=${thor}`;
+      let response= await fetch(url);
+      let data = await response.json();
+      return data;
+    } catch (error) {
+      console.log("Failed to fetch movie data :",error)
+      
+    }
+
+  
+}
+
 
 function App() {
 
